051c7b9e823fdf3764a74c8c246f7202f8842457
beebucketjosh <jl@beebucket.de>
2025-01-09 15:11:56 +0100

    Install missing test dependencies in release pipeline

---
+++ scripts/build_wheel.sh
@@ -0,0 +1,8 @@
+# !/bin/bash
+
+set -euo pipefail
+
+rm -rf ${DISTRIBUTION_DIR} build *.egg-info **/__pycache__
+mkdir -p ${DISTRIBUTION_DIR}
+echo "${VERSION}" > "${DISTRIBUTION_DIR}/VERSION.txt"
+pip wheel build --no-deps --no-input --no-binary :all: --wheel-dir ${DISTRIBUTION_DIR} .
